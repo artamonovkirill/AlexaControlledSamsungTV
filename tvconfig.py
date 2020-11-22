532d62a5bd2968f0be3ebd774baf9458449d9fca
Kirill Artamonov <artamonov.kirill@gmail.com>
2020-11-22 22:09:29 +0100

    :wrench: fixed TV IP from a new router

--- tvconfig.py
+++ tvconfig.py
@@ -4,7 +4,7 @@ volume_step_size = 5  #how much your tv volume should go up by when you say 'Ale
 
 tvs = [
     {
-        'host': "192.168.1.18", #ip address of tv
+        'host': "192.168.68.101", #ip address of tv
         'tv_model' : "UE40H6200", #9 digit tv model located on the back of your tv
         'tv_mac_address': "84:a4:66:fa:67:ef", #mac address of your tv
         'tv_name' : 'TV', #leave as TV to refrence this by just 'TV'. ex: 'Alexa, turn on the TV'.  Change to eg:'Kitchen TV' if you want to say 'Alexa turn on the kitchen TV', You cannot have multiple tvs have the same name
